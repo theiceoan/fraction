160c32e8e9b354d8adb7a987fd2adb26cccb70ce
theiceoan <87422366+UP2028963@users.noreply.github.com>
2024-09-09 16:20:19 +0100

    closing side panel working

--- client/src/header/Header.tsx
+++ client/src/header/Header.tsx
@@ -2,27 +2,19 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from './graphics/logo.svg';
-import { push as Menu } from 'react-burger-menu';
+import { push as Menu, State } from 'react-burger-menu';
 import './Header.css';
 import { MouseEvent, useEffect, useRef, useState } from 'react';
 
-interface MenuRef {
-	closeMenu: () => void;
-}
-
 export const Header = () => {
-	// const [isOpen, setIsOpen] = useState(false);
-	const menuRef = useRef<HTMLDivElement>(null);
+	const [isOpen, setIsOpen] = useState(false);
 
-  const handleCategoryClick = (e:MouseEvent) => {
-		// setIsOpen(prevState => !prevState)
-		// console.log(e.target)
-		console.log(menuRef.current)
-		if (menuRef.current) {
-			// console.log(menuRef.current)
-			(menuRef.current as any).closeMenu();
-		}
+  const handleStateChange = (state:State) => {
+		setIsOpen(state.isOpen)
 	};
+	const handleCategoryClick = (e:MouseEvent) => {
+		setIsOpen(false)
+	}
 
 	return (
 		<Navbar className='header' bg='dark' sticky='top'>
@@ -39,12 +31,18 @@ export const Header = () => {
 					<Nav.Link href="#work-with-us">Work With Us</Nav.Link>
 				</div>
 
-				<div onClick={handleCategoryClick} id='sidepanel-container'>
-					<Menu right pageWrapId={ "page-wrap"} outerContainerId={ "app-container"}>
-						<Nav.Link href="#our-story">Our Story</Nav.Link>
-						<Nav.Link href="#what-we-do">What We Do</Nav.Link>
-						<Nav.Link href="#our-work">Our Work</Nav.Link>
-						<Nav.Link href="#work-with-us">Work With Us</Nav.Link>
+				<div id='sidepanel-container'>
+					<Menu
+						right noOverlay
+						pageWrapId={ "page-wrap"}
+						outerContainerId={ "app-container"}
+						onStateChange={handleStateChange}
+						isOpen={isOpen}
+					>
+						<Nav.Link onClick={handleCategoryClick} href="#our-story">Our Story</Nav.Link>
+						<Nav.Link onClick={handleCategoryClick} href="#what-we-do">What We Do</Nav.Link>
+						<Nav.Link onClick={handleCategoryClick} href="#our-work">Our Work</Nav.Link>
+						<Nav.Link onClick={handleCategoryClick} href="#work-with-us">Work With Us</Nav.Link>
 					</Menu>
 				</div>
 			</Container>
