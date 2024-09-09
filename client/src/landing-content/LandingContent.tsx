4946f40c202f095b7b57a2616a6fe16e2c24b412
theiceoan <87422366+UP2028963@users.noreply.github.com>
2024-09-09 22:32:28 +0100

    styling changes for landing page

--- client/src/landing-content/LandingContent.tsx
+++ client/src/landing-content/LandingContent.tsx
@@ -1,6 +1,7 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import './LandingContent.css'
+import './LandingContent.css';
+import logo from './graphics/logo.svg';
 import { useEffect, useState } from 'react';
 import { benefitsOfOutsourcing } from './static/benefitsOfOutsourcing';
 
@@ -18,21 +19,28 @@ export const LandingContent = () => {
 	return (
 		<div className="landing-content-container">
 			<div className='background-graphic'></div>
+			<div className='corner-graphic'></div>
 			<main className="main-text-container">
 				<h1>PROVIDING <span className='highlight-gold'>HIGH-QUALITY </span>
 				TECH SOLUTIONS AT A <span className='highlight-blue'> FRACTION </span>
 				OF THE <span className='highlight-green'>COST</span>
 				</h1>
-				<h4>Tailored solutions that solve your problems
+				<h4 style={{paddingBottom: '1em'}}>Tailored solutions that solve your problems
 					and let you focus on delivering value and impact
 				</h4>
 				<Button variant='outline-info'>Want to Know More?</Button>
 			</main>
 			<main className='container-to-be-renamed'>
-				<Card bg='dark' text='white'>
+				<Card bg='light' text='black' style={{ opacity: '0.7'}}>
 					<Card.Body>
 						<Card.Title>{benefitsOfOutsourcing[currentBenefit].title}</Card.Title>
 						<Card.Text>{benefitsOfOutsourcing[currentBenefit].content}</Card.Text>
+						<Card.Img
+							src={logo}
+							width="10px"
+							alt={benefitsOfOutsourcing[currentBenefit].title}
+							className="benefit-svg"
+						/>
 					</Card.Body>
 				</Card>
 			</main>
--- client/src/landing-content/static/benefitsOfOutsourcing.ts
+++ client/src/landing-content/static/benefitsOfOutsourcing.ts
@@ -3,22 +3,22 @@ export const benefitsOfOutsourcing = [
     "id": 1,
     "title": "Cost Efficiency",
     "content": "Leverage the cost-effective advantage of African tech talent to optimize your budget and maximize ROI."
-  },
+    },
   {
     "id": 2,
     "title": "Skilled Workforce",
     "content": "Access a diverse pool of highly skilled developers with expertise in the latest technologies and innovations."
-  },
+    },
   {
     "id": 3,
     "title": "24/7 Productivity",
     "content": "Benefit from round-the-clock development cycles by tapping into Africa’s time zone advantage for continuous progress."
-  },
+    },
   {
     "id": 4,
     "title": "Innovation and Fresh Perspectives",
     "content": "Infuse your projects with innovative solutions and creative approaches from a dynamic and growing tech community."
-  },
+    },
   {
     "id": 5,
     "title": "Scalability and Flexibility",
