fcf94d82a86975ef900dcb59ca5e26d33cc1587c
theiceoan <87422366+UP2028963@users.noreply.github.com>
2024-09-21 21:09:15 +0100

    component moved to root (used by multiple components)

---
+++ client/src/Line.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Line = () => {
+	return (
+		<svg
+      viewBox="0 0 1024 1024"
+      fill="#61DAFB"
+      height="5em"
+      width="5em"
+    >
+      <path d="M904 476H120c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8z" />
+    </svg>
+	)
+}
