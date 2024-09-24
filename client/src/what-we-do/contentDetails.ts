f7a4d042fa93b88f24a15148ec705314aaedb397
theiceoan <87422366+UP2028963@users.noreply.github.com>
2024-09-24 19:36:30 +0100

    svgs plus static data for content

---
+++ client/src/what-we-do/contentDetails.ts
@@ -0,0 +1,44 @@
+export const contentDetails = [
+	{
+		id: 1,
+		title: "Mobile Application Development",
+		content: "",
+		svgPath: "./graphics/smartphone-shake.svg"
+	},
+	{
+		id: 2,
+		title: "Web Application Development",
+		content: "",
+		svgPath: "./graphics/codeSlash.svg"
+	},
+	{
+		id: 3,
+		title: "Custom Software Development",
+		content: "",
+		svgPath: "./graphics/layout-pin.svg"
+	},
+	{
+		id: 4,
+		title: "Cloud-Based Solutions",
+		content: "",
+		svgPath: "./graphics/cloud.svg"
+	},
+	{
+		id: 5,
+		title: "Cyber Security",
+		content: "",
+		svgPath: "./graphics/lock.svg"
+	},
+	{
+		id: 6,
+		title: "DevOps and Infrastructure Management",
+		content: "",
+		svgPath: "./graphics/devops-2.svg"
+	},
+	{
+		id: 7,
+		title: "UI/UX Design",
+		content: "",
+		svgPath: "./graphics/design.svg"
+	},
+]
